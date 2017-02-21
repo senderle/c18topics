302baff98f0d6f22fc81eaecd16d0f5994e99c5a
maecapozzi <maecapozzi@gmail.com>
2017-02-21 14:31:19 -0500

    continue pulling out logic into discrete functions

--- plot.js
+++ plot.js
@@ -1,5 +1,9 @@
+var setSVG = function(width, height) { 
+  return d3.select("#d3_selectable_force_directed_graph").append("svg").attr("width", width - 20).attr("height", height - 20);
+};
+
 var appendSVG = function(svg) { 
-  svg.append("svg:defs").selectAll("marker").data(["end"]).enter().append("svg:marker").attr("id", String).attr("viewBox", "0 -5 10 10").attr("refX", 20).attr("refY", -0.5).attr("markerWidth", 5).attr("markerHeight", 5).attr("orient", "auto").append("svg:path").attr("stroke-width", 1).attr("fill", 'seagreen').attr("d", "M0,-5L10,0L0,5");
+  return svg.append("svg:defs").selectAll("marker").data(["end"]).enter().append("svg:marker").attr("id", String).attr("viewBox", "0 -5 10 10").attr("refX", 20).attr("refY", -0.5).attr("markerWidth", 5).attr("markerHeight", 5).attr("orient", "auto").append("svg:path").attr("stroke-width", 1).attr("fill", 'seagreen').attr("d", "M0,-5L10,0L0,5");
 }; 
 
 var setRectangleAttributes = function(svg_graph, width, height) { 
@@ -16,10 +20,37 @@ var onResize = function(window) {
   d3.select(window).on("resize", resize); 
 };
 
-var redraw = function() { 
-   vis.attr("transform",
-     "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+var appendTopicLabel = function(node, opacity, node_r) {
+  var label = node.append("text").attr("class", "topic-click-label").attr("font-size", node_r).attr("x", node_r + 2).attr("dy", ".35em").attr("opacity", opacity).text(function(d) { return d.name });
+  return label
+};
+
+var circleClick = function() {
+  var thisnode = d3.select(this.parentNode);
+  var thislabel = thisnode.select(".topic-click-label");
+  if (thislabel.empty()) {
+    thislabel = appendTopicLabel(thisnode, 0, node_r);
+    thislabel.transition(500).attr("opacity", 1);
+  } else {
+    thislabel.transition(500).attr("opacity", 0).remove();
+  }
 }; 
+
+var linkArc = function(d) {
+  var dx = d.target.x - d.source.x,
+  dy = d.target.y - d.source.y,
+  dr = Math.sqrt(dx * dx + dy * dy);
+  return "M" + d.source.x + "," + d.source.y + 
+  "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+};  
+
+var linkLine = function(d) {
+  return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
+}; 
+
+var transform = function(d) {
+  return "translate(" + d.x + "," + d.y + ")";
+} 
 
 function selectableForceDirectedGraph() {
   var node_r = 10;
@@ -28,125 +59,69 @@ function selectableForceDirectedGraph() {
   var height = window.innerHeight;
   var xScale = d3.scale.linear().domain([0,width]).range([0,width]);
   var yScale = d3.scale.linear().domain([0,height]).range([0, height]);
-  var svg = d3.select("#d3_selectable_force_directed_graph").append("svg").attr("width", width - 20).attr("height", height - 20);
+  var svg = setSVG(width, height);
   var force = d3.layout.force().charge(-3000).gravity(0.65).linkDistance(20).size([width, height]);
   var zoomer = d3.behavior.zoom().scaleExtent([0.1,10]).x(xScale).y(yScale).on("zoom", redraw);
   var svg_graph = svg.append('svg:g').call(zoomer);
   var rect = setRectangleAttributes(svg_graph, width, height);
+  var vis = svg_graph.append("svg:g");
 
   appendSVG(svg);
  
-  
   function redraw() {
-    vis.attr("transform",
-     "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+    vis.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
   }
 
   resize(width, svg, rect);
 
-  onResize(window);
-                                 
+  onResize(window);                                
 
-                                  var vis = svg_graph.append("svg:g");
+  d3.json("graph.json", function(error, graph) {
+    for (var i = 0; i < graph.nodes.length; i++) {
+      graph.nodes[i].x = 500 + Math.sin(i) * 300;
+      graph.nodes[i].y = 300 + Math.cos(i) * 150;
+    }
 
-                                  d3.json("graph.json", function(error, graph) {
-                                    for (var i = 0; i < graph.nodes.length; i++) {
-                                      graph.nodes[i].x = 500 + Math.sin(i) * 300;
-                                      graph.nodes[i].y = 300 + Math.cos(i) * 150;
-                                    }
+  force.nodes(graph.nodes).links(graph.links).start();
 
-                                    force.nodes(graph.nodes)
-                                    .links(graph.links)
-                                    .start();
+  weight_max = Math.max.apply(null, graph.links.map(function(d) { return d.weight; })) / maxOpacity;
 
-                                    weight_max = Math.max.apply(null, graph.links.map(function(d) { return d.weight; })) / maxOpacity;
+  var link = vis.selectAll(".link").data(graph.links).enter().append("path").attr("class", "link").attr("opacity", function(d) { return d.weight / weight_max; }).attr("stroke-width", node_r / 6);
 
-                                    var link = vis.selectAll(".link")
-                                    .data(graph.links)
-                                    .enter().append("path")
-                                    .attr("class", "link")
-                                    .attr("opacity", function(d) { return d.weight / weight_max; })
-                                    .attr("stroke-width", node_r / 6);
+  if (graph.directed) {
+    link.attr("marker-end", "url(#end)");
+  }
 
-                                    if (graph.directed) {
-                                      link.attr("marker-end", "url(#end)");
-                                    }
+// Rather than creating separate groups for text labels and nodes, 
+// this just hides the circle for the "real" nodes and uses a separate
+// circle for the visible circle, which is drawn first because it
+// is appended first here. There might be a better solution, but 
+// I'm not sure what it is, and this works well enough.
 
-        // Rather than creating separate groups for text labels and nodes, 
-        // this just hides the circle for the "real" nodes and uses a separate
-        // circle for the visible circle, which is drawn first because it
-        // is appended first here. There might be a better solution, but 
-        // I'm not sure what it is, and this works well enough.
-        var undernode = vis.selectAll(".undernode")
-        .data(graph.nodes)
-        .enter().append("g")
-        .attr("class", "undernode")
+  var undernode = vis.selectAll(".undernode").data(graph.nodes).enter().append("g").attr("class", "undernode")
 
-        undernode.append("circle")
-        .attr("r", node_r)
+  undernode.append("circle").attr("r", node_r)
 
-        id_label = undernode.append("text")
-        .attr("class", "id-label")
-        .attr("font-size", node_r)
-        .attr("text-anchor", "middle")
-        .attr("dy", ".35em")
-        .text(function(d) { return d.id });
+  id_label = undernode.append("text").attr("class", "id-label").attr("font-size", node_r).attr("text-anchor", "middle").attr("dy", ".35em").text(function(d) { return d.id });
 
-        var node = vis.selectAll(".node")
-        .data(graph.nodes)
-        .enter().append("g")
-        .attr("class", "node");
+  var node = vis.selectAll(".node").data(graph.nodes).enter().append("g").attr("class", "node");
 
-        node.append("circle")
-        .attr("r", node_r)
-        .attr("opacity", 0)
-        .on("click", circle_click);
+  node.append("circle").attr("r", node_r).attr("opacity", 0).on("click", circleClick);
 
-        append_topic_label(node, 1);
+  appendTopicLabel(node, 1, node_r);
 
-        function append_topic_label(node, opacity) {
-          label = node.append("text")
-          .attr("class", "topic-click-label")
-          .attr("font-size", node_r)
-          .attr("x", node_r + 2)
-          .attr("dy", ".35em")
-          .attr("opacity", opacity)
-          .text(function(d) { return d.name });
-          return label
-        }
 
-        function circle_click() {
-          var thisnode = d3.select(this.parentNode);
-          var thislabel = thisnode.select(".topic-click-label");
-          if (thislabel.empty()) {
-            thislabel = append_topic_label(thisnode, 0);
-            thislabel.transition(500).attr("opacity", 1);
-          } else {
-            thislabel.transition(500).attr("opacity", 0).remove();
-          }
-        }
 
-        function link_arc(d) {
-          var dx = d.target.x - d.source.x,
-          dy = d.target.y - d.source.y,
-          dr = Math.sqrt(dx * dx + dy * dy);
-          return "M" + d.source.x + "," + d.source.y + 
-          "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
-        }
+        
 
-        function link_line(d) {
-          return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
-        }
 
-        if (graph.directed) {
-          link_path = link_arc
-        } else {
-          link_path = link_line
-        }
+  if (graph.directed) {
+    link_path = linkArc
+  } else {
+    link_path = linkLine
+  }
 
-        function transform(d) {
-          return "translate(" + d.x + "," + d.y + ")";
-        } 
+        
 
         function tick() {
           link.attr("d", link_path);
